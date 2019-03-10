a267b61bee033425e59b6e289e687ee217ec64e1
deanmixer <deanmixer@users.noreply.github.com>
2019-03-10 12:48:21 -0400

    Fixing Bootstrap and image pathing

--- server.js
+++ server.js
@@ -12,14 +12,17 @@ const db = require(path.join(__dirname, `models`));
 // const db = require('./models');
 
 // Serve static content for the app from the `public` directory //
-const directory_public = path.join(__dirname, `public`);
-app.use(express.static(directory_public));
+// const directory_public = path.join(__dirname, `public`);
+// app.use(express.static(directory_public));
+
+// app.use("/public", express.static(path.join(__dirname, "public")));
 
 // Set up Express to handle parsing data
 app.use(bodyParser.json()); 
 app.use(bodyParser.text()); 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(methodOverride(`method`))
+
 
 // Set Handlebars // 
 const routes = require('./routes/puppsController');
