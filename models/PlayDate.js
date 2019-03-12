a620f05f3c4b7808041c82b129752f75040ed3d4
helmi <helmitira@yahoo.com>
2019-03-12 19:35:51 -0400

    adding production and playdate .js

---
+++ models/PlayDate.js
@@ -0,0 +1,35 @@
+module.exports = function (sequelize, DataTypes) {
+  const playDate = sequelize.define('playDate', {
+    id: {
+      type: DataTypes.INTEGER(255),
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    match_sender: {
+      type: DataTypes.VARCHAR(255),
+      allowNull: false,
+    },
+    match_receiver: {
+      type: DataTypes.VARCHAR(255),
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    time: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    accepted: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  });
+  return playDate;
+}
