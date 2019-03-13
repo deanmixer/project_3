b1f47857f9677b799b6bf7c50d6701c02f57deb7
ttalgihon <vivithefool@gmail.com>
2019-03-12 20:11:42 -0400

    creating sql files

---
+++ seeds.sql
@@ -0,0 +1,18 @@
+-- Seeds for the SQL table, puppy_info
+USE puppy_info;
+INSERT INTO puppy_info (first_name,last_name,email,meet_location,puppy_name,puppy_gender,puppy_breed,puppy_personality,puppy_age,puppy_weight,img_url)
+VALUES
+('Darryl','Neal','DarNealx1@gmail.com','Rock Springs Dog Park','Jellybean','Female','Tibetan Mastiff','Independent, Reserved, Intelligent',8,140,'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12223604/Tibetan-Mastiff-On-White-01.jpg'),
+('Viktor', 'Clemons','ClemonsVx2@gmail.com','Chattapoochee Dog Park','Marbles','Male','Pomeranian','Inquisitive, Bold, Lively',3,5,'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12225627/Pomeranian-On-White-01.jpg'),
+('Maja','Boone','BooneMaja176@hotmail.com','Rodgers Bridge Park','Oreo','Male','Australian Shepherd','Smart, Work-Oriented, Exuberant',5,50,'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/03173132/Australian-Shepherd.1.jpg'),
+('Emile', 'Farrell','ParrellmEmile1995@yahoo.com','Ronald Reagan Park','Pickle','Female','Bichon Frise','Playful, Curious, Peppy',2,15,'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13000402/Bichon-Frise-On-White-03.jpg'),
+("Reese", 'Oneil','hutton@att.net','Brook Run Dog Park','Scoot','Male','Dalmatian','Dignified, Smart, Outgoing',3,50,'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234026/Dalmatian-On-White-01.jpg'),
+('Jamie', 'Hines','satch@aol.com','NewTown Dog Park','Nacho','Male','Dachshund','Friendly, Curious, Spunky',1,16,'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234109/Dachshund-On-White-03.jpg'),
+('Aden', 'Pineda','tkrotchko@hotmail.com','Rock Springs Dog Park','Potato','Female','French Bulldog','Adaptable, Playful, Smart',2,20,'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12195209/French-Bulldog-on-White-10-1-e1517511687435.jpg'),
+('Jorgie', 'Austin','twoflower@aol.com','Chattapoochee Dog Park','Noodle','Female','German Longhaired Pointer','Versatile, Calm, Family-Oriented',2,55,'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12210613/German-Longhaired-Pointer-on-White-01.jpg'),
+('Kia', 'Novak','Novak6969Kia@gmail.com','Rodgers Bridge Park','Bolt','Male','Greyhound','Gentle, Independent, Noble',12,65,'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12232417/Greyhound-MP.jpg'),
+('Maddy', 'Penn','jigsaw@msn.com','Brook Run Dog Park','Flash','Male','Jindo','Alert, Intelligent, Bold',11,40,'https://images.ctfassets.net/w00x6p5qkejz/4IQHKDVicUw6k8YUUICmom/099cc07fe25a5d10c6f6ef42ae3035ef/Korean_Jindo_300x300-1.jpg'),
+('Tobi', 'Bright','TobiB@hotmail.com','Waggy World Paw Park','Luna','Female','Mudi','Loyal, Intelligent, Active',10,14,'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12230639/Mudi-On-White-01.jpg'),
+('Kristen', 'Weiss','KrisWeiss101@gmail.com','Oakhurst Dog Park','Spot','Female','Parson Russell Terrier','Friendly, Clever, Athletic',7,17,'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12211654/Parson-Russell-Terrier-on-White-05.jpg'),
+('Mae', 'England','EnglandSoccerMae15@msn.com','NewTown Dog Park','Otis','Male','Corgi','Affectionate, Smart, Alert',5,30,'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12225906/Pembroke-Welsh-Corgi-On-White-05.jpg'),
+('Kyan', 'Wall','moonlapse@outlook.com','Ronald Reagan Park','Max','Male','Pug','Charming, Mischievous, Loving',3,14,'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12225349/Pug-On-White-03.jpg');
