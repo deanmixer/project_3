47292b5a8645593060c355ecdf71b079b302d6bc
ttalgihon <31228739+ttalgihon@users.noreply.github.com>
2019-03-14 18:51:20 -0400

    heroku config (#19)

--- routes/puppsController.js
+++ routes/puppsController.js
@@ -1,5 +1,5 @@
 const express = require(`express`);
-const router = express.router();
+const router = express.Router();
 const pupps = require(`../models/pupps.js`);
 
 router.get('/', function(req, res) {
