1a2a972d3f7b16fff3562186e3e2a94a51e8c53c
ttalgihon <vivithefool@gmail.com>
2019-03-14 20:48:03 -0400

    fixing eslint, commenting out the sequelize files

--- models/PlayDate.js
+++ models/PlayDate.js
@@ -1,37 +1,37 @@
-module.exports = function(sequelize, DataTypes) {
-  const playDate = sequelize.define('playDate', {
-    id: {
-      type: DataTypes.INTEGER(255),
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    match_sender: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    match_receiver: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    time: {
-      type: DataTypes.TIME,
-      allowNull: false,
-    },
-    location: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    accepted: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-  });
-  return playDate;
+// module.exports = function(sequelize, DataTypes) {
+//   const playDate = sequelize.define('playDate', {
+//     id: {
+//       type: DataTypes.INTEGER(255),
+//       allowNull: false,
+//       autoIncrement: true,
+//       primaryKey: true,
+//     },
+//     match_sender: {
+//       type: DataTypes.STRING(255),
+//       allowNull: false,
+//     },
+//     match_receiver: {
+//       type: DataTypes.STRING(255),
+//       allowNull: false,
+//     },
+//     date: {
+//       type: DataTypes.DATE,
+//       allowNull: false,
+//     },
+//     time: {
+//       type: DataTypes.TIME,
+//       allowNull: false,
+//     },
+//     location: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     accepted: {
+//       type: DataTypes.BOOLEAN,
+//       allowNull: false,
+//     },
+//   });
+//   return playDate;
 
-}
+// }
 
