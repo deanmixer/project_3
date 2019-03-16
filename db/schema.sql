02c99f6e053564d6498628d7198ec2472f2f5c10
deanmixer <deanmixer@users.noreply.github.com>
2019-03-16 07:47:40 -0400

    heroku updates

--- db/schema.sql
+++ db/schema.sql
@@ -31,3 +31,7 @@ CREATE TABLE playDate
    date DATE NOT NULL,
    PRIMARY KEY (id)
    );
+   
+INSERT INTO playDate (mail1, mail2, location, accepted, date) VALUES ('deanmixer3@yahoo.com', 'deanmixer69@yahoo.com', 'Piedmont Park', false, '2019-03-28'	);
+INSERT INTO playDate (mail1, mail2, location, accepted, date) VALUES ('helmitira@yahoo.com', 'deanmixer69@yahoo.com', 'Morningside', false, '2019-03-21');
+INSERT INTO playDate (mail1, mail2, location, accepted, date) VALUES ('deanmixer@yahoo.com', 'deanmixer69@yahoo.com', 'Piedmont Park', false, '2019-03-14');
