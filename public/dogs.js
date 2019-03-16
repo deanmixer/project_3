92a2dc4dd2b74614b763ba7a491d2f0bbab598d8
deanmixer <deanmixer@users.noreply.github.com>
2019-03-15 23:52:27 -0400

    Updates

--- public/dogs.js
+++ public/dogs.js
@@ -1,30 +1,11 @@
-$('.carousel').carousel();
+$('.carousel').carousel();  
 
-$(document).ready(function() {
+$(document).ready(function() { 
     if (document.cookie) {
-    console.log("Cookie")
         $("#cookie").addClass("show");
         $("#noCookie").addClass("hide");
-        // $("#cookie").removeClass("hide");  
     } else {
-    console.log("No cookie")
         $("#cookie").addClass("hide");
         $("#noCookie").addClass("show");
-        // $("#noCookie").removeClass("hide"); 
     };
 });
-
-// window.onload = (function() {
-//     $('.carousel').carousel();
-//     if (document.cookie) {
-//     console.log("Cookie")
-//         // $("#cookie").addClass("show");
-//         // $("#noCookie").addClass("hide");
-//         $("#cookie").removeClass("hide");  
-//     } else {
-//     console.log("No cookie")
-//         // $("#cookie").addClass("hide");
-//         // $("#noCookie").addClass("show");
-//         $("#noCookie").removeClass("hide"); 
-//     };
-// });
